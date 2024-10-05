87fdacd1d2933061358c6eabd81005a097c63f86
Shane McDonald <me@shanemcd.com>
2024-10-05 19:19:17 -0400

    Tinkering

--- roles/fedora_iso/files/start.sh
+++ roles/fedora_iso/files/start.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-# Check for FEDORA_ISO_LINK environment variable
-if [ -z "${FEDORA_ISO_LINK}" ]; then
-  echo "Error: FEDORA_ISO_LINK environment variable is not set."
+set -x
+
+# Check for FEDORA_ISO_PATH environment variable
+if [ -z "${FEDORA_ISO_PATH}" ]; then
+  echo "Error: FEDORA_ISO_PATH environment variable is not set."
   exit 1
 fi
 
-# Default output directory, can be overridden by FEDORA_ISO_OUTPUT_DIR env var
-OUTPUT_DIR="/tmp"
+FEDORA_ISO_DIR=$(dirname "${FEDORA_ISO_PATH}")
+FEDORA_ISO_FILE_NAME=$(basename "${FEDORA_ISO_PATH}")
 
-# Check if custom output directory is specified in the environment variables
-if [ -n "${FEDORA_ISO_OUTPUT_DIR}" ]; then
-  OUTPUT_DIR="${FEDORA_ISO_OUTPUT_DIR}"
-fi
-
-# Extract file name from URL using parameter expansion
-FILE_NAME=$(basename "${FEDORA_ISO_LINK}")
-
-wget --directory-prefix "${FEDORA_ISO_OUTPUT_DIR}" "$FEDORA_ISO_LINK"
-
-# Print completion message
-echo "Fedora ISO downloaded successfully to ${OUTPUT_DIR}/${FILE_NAME}"
-
-sleep infinity
+mkksiso --ks /context/ks.cfg ${FEDORA_ISO_DIR}/${FEDORA_ISO_FILE_NAME} ${FEDORA_ISO_DIR}/ks-${FEDORA_ISO_FILE_NAME}
